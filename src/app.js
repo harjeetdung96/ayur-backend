4e07d1bde02d176a45cd4e355c4aa7c83a8a603e
Harjeet Singh <harjeets.offsureit@gmail.com>
2023-05-16 17:51:35 +0530

    Fixed CORS Issue for API

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ require('./db/conn');
 const app = express();
 const router = require('./routers/customer');
 const auth = require('./routers/authentication');
-
+var cors = require('cors')
 
 app.use(express.json());
+app.use(cors())
 app.use(router);
 app.use(auth);
 
