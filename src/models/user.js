91fcf6ab213dbb821a1bcb499da68fbf053fca49
Harjeet Singh <harjeets.offsureit@gmail.com>
2023-06-14 15:21:23 +0530

    new code

--- src/models/user.js
+++ src/models/user.js
@@ -23,36 +23,36 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-UserSchema.pre("save", function (next) {
-    const user = this
+// UserSchema.pre("save", function (next) {
+//     const user = this
 
-    if (user.isModified("password") || user.isNew) {
-        bcrypt.genSalt(10, function (saltError, salt) {
-            if (saltError) {
-                return next(saltError)
-            } else {
-                bcrypt.hash(user.password, salt, function (hashError, hash) {
-                    if (hashError) {
-                        return next(hashError)
-                    }
-                    user.password = hash
-                    next()
-                })
-            }
-        })
-    } else {
-        return next()
-    }
-})
+//     if (user.isModified("password") || user.isNew) {
+//         bcrypt.genSalt(10, function (saltError, salt) {
+//             if (saltError) {
+//                 return next(saltError)
+//             } else {
+//                 bcrypt.hash(user.password, salt, function (hashError, hash) {
+//                     if (hashError) {
+//                         return next(hashError)
+//                     }
+//                     user.password = hash
+//                     next()
+//                 })
+//             }
+//         })
+//     } else {
+//         return next()
+//     }
+// })
 
-UserSchema.methods.comparePassword = async function (password, db_password) {
-    const match = await bcrypt.compare(password, db_password);
-    if (match) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
+// UserSchema.methods.comparePassword = async function (password, db_password) {
+//     const match = await bcrypt.compare(password, db_password);
+//     if (match) {
+//         return 1;
+//     }
+//     else {
+//         return 0;
+//     }
+// }
 
 module.exports = mongoose.model("User", UserSchema)
